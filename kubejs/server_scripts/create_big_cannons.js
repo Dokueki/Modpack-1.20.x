cd04b4223c429508c4d73695baeeb39cfc7859ac
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2024-04-30 22:26:13 +0300

    Changed basin foundry recipe

---
+++ kubejs/server_scripts/create_big_cannons.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(event => {
+    event.replaceInput({ output: 'createbigcannons:basin_foundry_lid' },
+        'create:andesite_alloy',
+        '#forge:metal/wrought_iron'
+    )
+})
