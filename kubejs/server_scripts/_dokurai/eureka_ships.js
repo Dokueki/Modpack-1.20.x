f1be04255ae84a372b644342aac0dc2dff068bf5
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2024-05-13 16:45:56 +0300

    Fixed ballast craft

--- kubejs/server_scripts/_dokurai/eureka_ships.js
+++ kubejs/server_scripts/_dokurai/eureka_ships.js
@@ -1,15 +1,26 @@
 ServerEvents.recipes(event => {
-    // заменил 'minecraft:blast_furnace' на 'gtceu:steel_brick_casing' и починил ванильные камни
+    
+    event.remove({ output: 'vs_eureka:engine' })
     event.shaped('vs_eureka:engine', [
         'AAA',
         'CED',
         'BBB'
     ], {
-        A: 'tfc:rock/row/*',
+        A: 'tfc:rock/raw/*',
         B: 'tfc:rock/smooth/*',
         C: 'gtceu:wrought_iron_ingot',
         D: 'forge:glass_panes',
         E: 'gtceu:steel_brick_casing'
         
+    })
+    
+    event.remove({ output: 'vs_eureka:ballast' })
+    event.shaped('', [
+        'ABA',
+        'B B',
+        'ABA'
+    ], {
+        A: 'tfc:rock/raw/*',
+        B: 'tfc:rock/cobbled/*'
     })
 })
